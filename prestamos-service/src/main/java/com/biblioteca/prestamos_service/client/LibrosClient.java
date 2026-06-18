72234f485ba2705488059831321e6f14649d54ae
mamba-kathryn <k.morat@duocuc.cl>
2026-06-18 12:35:18 -0400

    Update LibrosClient.java

--- prestamos-service/src/main/java/com/biblioteca/prestamos_service/client/LibrosClient.java
+++ prestamos-service/src/main/java/com/biblioteca/prestamos_service/client/LibrosClient.java
@@ -1,1 +1,11 @@
+package com.biblioteca.prestamos_service.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "libros-service", url = "http://localhost:8085")
+public interface LibrosClient {
+    @GetMapping("/libros/libro/{libroId}")
+    Object verificarDisponibilidad(@PathVariable String libroId);
+}
